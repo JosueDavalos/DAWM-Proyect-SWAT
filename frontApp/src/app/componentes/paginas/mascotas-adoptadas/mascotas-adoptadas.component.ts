fbb60988d6b865b309da854e63e1471611524a6f
unknown <eunice_galvez_10@hotmail.com>
2020-07-07 03:31:28 -0500

    First version of Mascotas adoptabas screen

--- frontApp/src/app/componentes/paginas/mascotas-adoptadas/mascotas-adoptadas.component.ts
+++ frontApp/src/app/componentes/paginas/mascotas-adoptadas/mascotas-adoptadas.component.ts
@@ -1,11 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Animal {
+  nombre: string;
+  tipo: string;
+  edad: number;
+  raza: string;
+  sexo: string;
+  picture: string;}
+const ANIMALES: Animal[] = [
+  {
+    nombre: 'Juanito',
+    tipo: 'gato',
+    edad: 1,
+    raza: 'mestizo',
+    sexo: 'Macho',
+    picture: 'gato'
+  },
+  {
+    nombre: 'Amanda',
+    tipo: 'gato',
+    edad: 6,
+    raza: 'mestizo',
+    sexo: 'Hembra',
+    picture: 'gato'
+
+  },
+  {
+    nombre: 'Mota',
+    tipo: 'perro',
+    edad: 1,
+    raza: 'mestizo',
+    sexo: 'Macho',
+    picture: 'perro'
+  },
+  {
+    nombre: 'Claudio',
+    tipo: 'perro',
+    edad: 2,
+    raza: 'mestizo',
+    sexo: 'Macho',
+    picture: 'perro'
+  }
+];
 @Component({
   selector: 'app-mascotas-adoptadas',
   templateUrl: './mascotas-adoptadas.component.html',
   styleUrls: ['./mascotas-adoptadas.component.css']
 })
 export class MascotasAdoptadasComponent implements OnInit {
+  animales = ANIMALES;
 
   constructor() { }
 
@@ -13,3 +56,8 @@ export class MascotasAdoptadasComponent implements OnInit {
   }
 
 }
+
+
+
+
+
