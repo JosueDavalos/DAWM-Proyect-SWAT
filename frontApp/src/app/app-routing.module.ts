3f41bdc7b399170f8bd4048da8634a8c1bf8db12
unknown <eunice_galvez_10@hotmail.com>
2020-07-11 20:20:08 -0500

    improve navigation and css

--- frontApp/src/app/app-routing.module.ts
+++ frontApp/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { AdoptarComponent } from './componentes/paginas/adoptar/adoptar.component';
-import { PonerAdopcionComponent } from './componentes/paginas/poner-adopcion/poner-adopcion.component';
 import { MascotasAdoptadasComponent } from './componentes/paginas/mascotas-adoptadas/mascotas-adoptadas.component';
 import { OrganizacionesComponent } from './componentes/paginas/organizaciones/organizaciones.component';
 import { QuienesSomosComponent } from './componentes/paginas/quienes-somos/quienes-somos.component';
@@ -17,7 +16,7 @@ import { ReportesComponent } from './componentes/admin/reportes/reportes.compone
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'adoptar', component: AdoptarComponent },
-  { path: 'ponerAdopcion', component: PonerAdopcionComponent },
+  { path: 'ponerAdopcion', component: GestionProcesoComponent }, // component: PonerAdopcionComponent },
   { path: 'adoptadas', component: MascotasAdoptadasComponent },
   { path: 'organizaciones', component: OrganizacionesComponent },
   { path: 'quienesSomos', component: QuienesSomosComponent },
--- frontApp/src/app/componentes/paginas/mascotas-adoptadas/mascotas-adoptadas.component.ts
+++ frontApp/src/app/componentes/paginas/mascotas-adoptadas/mascotas-adoptadas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AnimalService } from '../../../servicios/animal.service';
 import { Animal } from '../../models/animal';
+
 @Component({
   selector: 'app-mascotas-adoptadas',
   templateUrl: './mascotas-adoptadas.component.html',
@@ -8,16 +9,14 @@ import { Animal } from '../../models/animal';
 })
 export class MascotasAdoptadasComponent implements OnInit {
   animals: Animal[];
-  constructor(private animalService: AnimalService) { }
+  constructor(
+    private animalService: AnimalService
+    ) { }
 
   ngOnInit(): void {
     this.getAnimals();
-    console.log('animals');
-    console.log(this.animals);
   }
   getAnimals(): void {
-    // The subscribe() method passes the emitted array to the callback,
-    // which sets the component's animals property.
     this.animalService.getAnimals()
     .subscribe(animals => this.animals = animals);
   }
