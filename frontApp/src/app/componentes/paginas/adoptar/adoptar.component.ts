7b5459fe2409dfce8e66e8e4794096c432e0888d
unknown <eunice_galvez_10@hotmail.com>
2020-07-13 19:43:25 -0500

    Add details for each animal in adoptar screen

--- frontApp/src/app/componentes/paginas/adoptar/adoptar.component.ts
+++ frontApp/src/app/componentes/paginas/adoptar/adoptar.component.ts
@@ -1,94 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { ANIMALS } from '../../../mock-animals';
+import { Animal } from '../../models/animal';
+import { AnimalService } from '../../../servicios/animal.service';
+
 
 @Component({
   selector: 'app-adoptar',
   templateUrl: './adoptar.component.html',
   styleUrls: ['./adoptar.component.css']
 })
+
 export class AdoptarComponent implements OnInit {
-  tipos : Object[] = [
-    {
-      id : 0,
-      tipo : 'Perro'
-    },
-    {
-      id : 1,
-      tipo : 'Gato'
-    },
-    {
-      id : 0,
-      tipo : 'Loro'
-    }
-  ];
-
-  razas : Object[] = [
-    {
-      id : 0,
-      raza : 'Schnauzer',
-    },
-    {
-      id : 1,
-      raza : 'Pug',
-    },
-    {
-      id : 2,
-      raza : 'Persian',
-    },
-    {
-      id : 2,
-      raza : 'Siames',
-    }
-  ];
-
-  etapas : Object[] = [
-    {
-      id : 0,
-      etapa : 'Cachorro'
-    },
-    {
-      id: 1,
-      etapa : 'Adulto'
-    },
-    {
-      id : 2,
-      etapa : 'Viejo'
-    }
-  ];
-
-  sectores : Object[] = [
-    {
-      id : 0,
-      sector : 'Duran',
-    },
-    {
-      id : 1,
-      sector : 'Prosperina',
-    },
-    {
-      id : 2,
-      sector : 'Sauces'
-    }
-  ];
-
-  mascotas = ANIMALS;
-
-  toggleDetails(id){
-    if(document.getElementById(id).getAttribute("class") == "content"){
-      document.getElementById(id).className += " content-shown";
-    }else{
-      document.getElementById(id).className = "content";
-    }  
-  }
-
-  adoptar(){
-    
-  }
-
-  constructor() { }
+  animals: Animal[];
+  tipos: string[] = ['Perro', 'Gato', 'Loro'];
+  razas: string[] = ['Schnauzer', 'Pug', 'Persian', 'Siames'];
+  etapas: string[] = ['Cachorro', 'Adulto', 'Viejo'];
+  sectores: string[] = ['Duran', 'Prosperina', 'Sauces'];
+  constructor(
+    private animalService: AnimalService
+    ) { }
 
   ngOnInit(): void {
-
+    this.getAnimals();
   }
-
+  getAnimals(): void {
+    this.animalService.getAnimals()
+    .subscribe(animals => this.animals = animals);
+  }
 }
