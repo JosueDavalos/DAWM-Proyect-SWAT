078609b61bbdc0e8369b5a202d9f27bf2434de2d
Erickds Pulla <erickpullads@hotmail.com>
2020-09-16 10:12:15 -0500

    Descarga de archivo CSV completada

--- frontApp/src/app/componentes/admin/reporte-adopcion/reporte-adopcion.component.ts
+++ frontApp/src/app/componentes/admin/reporte-adopcion/reporte-adopcion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Restangular } from 'ngx-restangular';
+import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-reporte-adopcion',
   templateUrl: './reporte-adopcion.component.html',
@@ -7,12 +8,26 @@ import { Restangular } from 'ngx-restangular';
 })
 export class ReporteAdopcionComponent implements OnInit {
   adopciones = [];
+  adopcionesCSV = [];
   entero = 0;
   constructor(private restangular: Restangular) { }
 
   ngOnInit(): void {
     this.restangular.all("adopcion").getList().subscribe(adopciones => {
       this.adopciones = adopciones;
+      for(let adopcion of adopciones){
+        if(adopcion.animal.nombre != undefined)
+          this.adopcionesCSV.push({
+            nombre: adopcion.animal.nombre,
+            edad: adopcion.animal.edad,
+            raza: adopcion.animal.raza,
+            tipo: adopcion.animal.tipo,
+            cedula: adopcion.usuario.cedula,
+            nombreUser: adopcion.usuario.nombre,
+            apellido: adopcion.usuario.apellido,
+            sexo: adopcion.usuario.sexo,
+          });
+      }
       this.entero++;
     });;
   }
@@ -21,5 +36,17 @@ export class ReporteAdopcionComponent implements OnInit {
     console.log(this.adopciones);
     
   }
+
+  downloadFile() {
+    let data = this.adopcionesCSV;
+    const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+    csv.unshift(header.join(','));
+    let csvArray = csv.join('\r\n');
+
+    var blob = new Blob([csvArray], {type: 'text/csv' })
+    saveAs(blob, "myFile.csv");
+}
 
 }
