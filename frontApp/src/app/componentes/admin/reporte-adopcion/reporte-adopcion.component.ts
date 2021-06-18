ad0d20e67ad5e5be53242c23ce482f4a1f31eb09
Josue Davalos <jedavalo@espol.edu.ec>
2021-06-18 13:37:56 -0500

    Finish

--- frontApp/src/app/componentes/admin/reporte-adopcion/reporte-adopcion.component.ts
+++ frontApp/src/app/componentes/admin/reporte-adopcion/reporte-adopcion.component.ts
@@ -1,52 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { Restangular } from 'ngx-restangular';
+import { Component, OnInit } from "@angular/core";
+import { Restangular } from "ngx-restangular";
 // import { saveAs } from 'file-saver';
 @Component({
-  selector: 'app-reporte-adopcion',
-  templateUrl: './reporte-adopcion.component.html',
-  styleUrls: ['./reporte-adopcion.component.css']
+  selector: "app-reporte-adopcion",
+  templateUrl: "./reporte-adopcion.component.html",
+  styleUrls: ["./reporte-adopcion.component.css"],
 })
 export class ReporteAdopcionComponent implements OnInit {
   adopciones = [];
   adopcionesCSV = [];
   entero = 0;
-  constructor(private restangular: Restangular) { }
+  constructor(private restangular: Restangular) {}
 
   ngOnInit(): void {
-    this.restangular.all("adopcion").getList().subscribe(adopciones => {
-      this.adopciones = adopciones;
-      for(let adopcion of adopciones){
-        if(adopcion.animal.nombre != undefined)
-          this.adopcionesCSV.push({
-            nombre: adopcion.animal.nombre,
-            edad: adopcion.animal.edad,
-            raza: adopcion.animal.raza,
-            tipo: adopcion.animal.tipo,
-            cedula: adopcion.usuario.cedula,
-            nombreUser: adopcion.usuario.nombre,
-            apellido: adopcion.usuario.apellido,
-            sexo: adopcion.usuario.sexo,
-          });
-      }
-      this.entero++;
-    });;
+    this.restangular
+      .all("adopcion")
+      .getList()
+      .subscribe((adopciones) => {
+        this.adopciones = adopciones;
+        for (let adopcion of adopciones) {
+          if (adopcion.animal.nombre != undefined)
+            this.adopcionesCSV.push({
+              nombre: adopcion.animal.nombre,
+              edad: adopcion.animal.edad,
+              raza: adopcion.animal.raza,
+              tipo: adopcion.animal.tipo,
+              cedula: adopcion.usuario.cedula,
+              nombreUser: adopcion.usuario.nombre,
+              apellido: adopcion.usuario.apellido,
+              sexo: adopcion.usuario.sexo,
+            });
+        }
+        this.entero++;
+      });
   }
 
-  doRequest(){
+  doRequest() {
     console.log(this.adopciones);
-    
   }
 
   downloadFile() {
     let data = this.adopcionesCSV;
-    const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+    const replacer = (key, value) => (value === null ? "" : value); // specify how you want to handle null values here
     const header = Object.keys(data[0]);
-    let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
-    csv.unshift(header.join(','));
-    let csvArray = csv.join('\r\n');
+    let csv = data.map((row) =>
+      header
+        .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+        .join(",")
+    );
+    csv.unshift(header.join(","));
+    let csvArray = csv.join("\r\n");
 
-    var blob = new Blob([csvArray], {type: 'text/csv' })
-    saveAs(blob, "myFile.csv");
-}
-
+    var blob = new Blob([csvArray], { type: "text/csv" });
+    // saveAs(blob, "myFile.csv");
+  }
 }
